76b4326fd52a8ceb1658f879fd569f814e03b55d
Joseph Chima <josephchima@Josephs-MacBook-Pro.local>
2021-09-29 07:52:24 -0600

    First commit

---
+++ src/main/java/com/jchimasubmittable/containercidemo/ContainerCiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jchimasubmittable.containercidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerCiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerCiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jchimasubmittable/containercidemo/ContainerCiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jchimasubmittable.containercidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerCiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
